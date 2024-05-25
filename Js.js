6b925feda35df0832ccbba278a0df5f610a5ec41
liana kolodgeeva <liana.kolodgeeva@gmail.com>
2024-05-25 09:38:52 +0200

    test

--- Js.js
+++ Js.js
@@ -1,37 +1,37 @@
 function checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer) {
     if (userAnswer === correctAnswer) {
-         stylesIfCorrectAnswer();    
-        continueButtonIfAnswerIsCorrect();  
+        stylesIfCorrectAnswer();
+        continueButtonIfAnswerIsCorrect();
     } else {
         stylesIfWrongAnswer();
         continueButtonIfAnswerIsWrong();
     }
 }
-    function stylesIfCorrectAnswer() {
-        document.getElementById("outputTests").style.backgroundColor = "#D9EEE1";
-        document.getElementById("outputTests").style.paddingLeft = "5%";
-        document.getElementById("outputTests").style.paddingTop = "5%"
-        document.getElementById("outputTests").style.height = "265px"
-        document.getElementById("outputTests").style.margin = "10px"
-        document.getElementById("outputTests").style.padding - "10px"
-    }
+function stylesIfCorrectAnswer() {
+    document.getElementById("outputTests").style.backgroundColor = "#D9EEE1";
+    document.getElementById("outputTests").style.paddingLeft = "5%";
+    document.getElementById("outputTests").style.paddingTop = "5%"
+    document.getElementById("outputTests").style.height = "265px"
+    document.getElementById("outputTests").style.margin = "10px"
+    document.getElementById("outputTests").style.padding - "10px"
+}
 
-    function stylesIfWrongAnswer() {
-        document.getElementById("outputTests").style.backgroundColor = "#FFC0C7";
-        document.getElementById("outputTests").style.paddingLeft = "5%";
-        document.getElementById("outputTests").style.paddingTop = "5%"
-        document.getElementById("outputTests").style.height = "265px"
-        document.getElementById("outputTests").style.margin = "10px"
-        document.getElementById("outputTests").style.padding - "10px"
-    }
+function stylesIfWrongAnswer() {
+    document.getElementById("outputTests").style.backgroundColor = "#FFC0C7";
+    document.getElementById("outputTests").style.paddingLeft = "5%";
+    document.getElementById("outputTests").style.paddingTop = "5%"
+    document.getElementById("outputTests").style.height = "265px"
+    document.getElementById("outputTests").style.margin = "10px"
+    document.getElementById("outputTests").style.padding - "10px"
+}
 
-    function continueButtonIfAnswerIsCorrect() {
-        document.getElementById("outputTests").innerHTML = '<h3 style = "color:#04AA6D"> CORRECT!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
-    }
+function continueButtonIfAnswerIsCorrect() {
+    document.getElementById("outputTests").innerHTML = '<h3 style = "color:#04AA6D"> CORRECT!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
+}
 
-    function continueButtonIfAnswerIsWrong() {
-        document.getElementById("outputTests").innerHTML = '<h3 style = "color:#b94a48"> WRONG!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
-    }
+function continueButtonIfAnswerIsWrong() {
+    document.getElementById("outputTests").innerHTML = '<h3 style = "color:#b94a48"> WRONG!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
+}
 
 function checkingTheAnswerInOutPutTest1() {
     let corectAnsverInOutPutTest1 = "window.alert";
