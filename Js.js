16037260f6cddb7776877c697c9b65cf0bfbc9ce
Liana_Kolodzheieva@example,com <liana.kolodgeeva@gmail.com>
2024-05-27 14:20:59 +0200

    button

--- Js.js
+++ Js.js
@@ -1,40 +1,55 @@
 function checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer) {
-    if (userAnswer === correctAnswer) {
-        stylesIfCorrectAnswer();
-        continueButtonIfAnswerIsCorrect();
-    } else {
-        stylesIfWrongAnswer();
-        continueButtonIfAnswerIsWrong();
-    }
+  if (userAnswer === correctAnswer) {
+    stylesIfCorrectAnswer();
+    document.getElementById("task").innerHTML =
+      '<h3 style = "color:#04AA6D"> CORRECT!';
+    document.getElementById("ex").innerHTML = " ";
+    document.getElementById("outputTest").innerHTML = " ";
+    document.getElementById("outputTest").style.backgroundColor = "#D9EEE1";
+  } else {
+    stylesIfWrongAnswer();
+    document.getElementById("task").innerHTML =
+      '<h3 style = "color:#b94a48"> WRONG!';
+    document.getElementById("ex").innerHTML = " ";
+    document.getElementById("outputTest").innerHTML = " ";
+    document.getElementById("outputTest").style.backgroundColor = "#FFC0C7";
+  }
 }
 function stylesIfCorrectAnswer() {
-    document.getElementById("outputTests").style.backgroundColor = "#D9EEE1";
-    document.getElementById("outputTests").style.paddingLeft = "5%";
-    document.getElementById("outputTests").style.paddingTop = "5%"
-    document.getElementById("outputTests").style.height = "265px"
-    document.getElementById("outputTests").style.margin = "10px"
-    document.getElementById("outputTests").style.padding - "10px"
+  document.getElementById("outputTests").style.backgroundColor = "#D9EEE1";
+  document.getElementById("outputTests").style.paddingLeft = "5%";
+  document.getElementById("outputTests").style.paddingTop = "5%";
+  document.getElementById("outputTests").style.height = "265px";
+  document.getElementById("outputTests").style.margin = "10px";
+  document.getElementById("outputTests").style.padding - "10px";
 }
 
 function stylesIfWrongAnswer() {
-    document.getElementById("outputTests").style.backgroundColor = "#FFC0C7";
-    document.getElementById("outputTests").style.paddingLeft = "5%";
-    document.getElementById("outputTests").style.paddingTop = "5%"
-    document.getElementById("outputTests").style.height = "265px"
-    document.getElementById("outputTests").style.margin = "10px"
-    document.getElementById("outputTests").style.padding - "10px"
-}
-
-function continueButtonIfAnswerIsCorrect() {
-    document.getElementById("outputTests").innerHTML = '<h3 style = "color:#04AA6D"> CORRECT!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
-}
-
-function continueButtonIfAnswerIsWrong() {
-    document.getElementById("outputTests").innerHTML = '<h3 style = "color:#b94a48"> WRONG!' + "</br>" + "</br>" + "</br>" + "</br>" + '<button>Continue</button>'
+  document.getElementById("outputTests").style.backgroundColor = "#FFC0C7";
+  document.getElementById("outputTests").style.paddingLeft = "5%";
+  document.getElementById("outputTests").style.paddingTop = "5%";
+  document.getElementById("outputTests").style.height = "265px";
+  document.getElementById("outputTests").style.margin = "10px";
+  document.getElementById("outputTests").style.padding - "10px";
 }
 
 function checkingTheAnswerInOutPutTest1() {
-    let corectAnsverInOutPutTest1 = "window.alert";
-    let usersAnsverInOutPutTest1 = document.getElementById("answer").value;
-    checkingTheAnswerIfThereIs1Answer(usersAnsverInOutPutTest1, corectAnsverInOutPutTest1)
+  let corectAnsverInOutPutTest1 = "window.alert";
+  let usersAnsverInOutPutTest1 = document.getElementById("answer").value;
+  checkingTheAnswerIfThereIs1Answer(
+    usersAnsverInOutPutTest1,
+    corectAnsverInOutPutTest1
+  );
+  document.getElementById("butt").innerHTML =
+    "<button onclick = 'stylesForNewQ()' id = 'button'>Continue</button>";
+}
+
+function stylesForNewQ() {
+  document.getElementById("outputTests").style.backgroundColor =
+    "rgb(245, 246, 250)";
+  document.getElementById("outputTests").style.paddingLeft = "5%";
+  document.getElementById("outputTests").style.paddingTop = "5%";
+  document.getElementById("outputTests").style.height = "265px";
+  document.getElementById("outputTests").style.margin = "10px";
+  document.getElementById("outputTests").style.padding - "10px";
 }
