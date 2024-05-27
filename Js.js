3249f78a324e892f81c111bc9921e50720c09310
Liana_Kolodzheieva@example,com <liana.kolodgeeva@gmail.com>
2024-05-27 15:47:55 +0200

    finish tests in output

--- Js.js
+++ Js.js
@@ -34,22 +34,52 @@ function stylesIfWrongAnswer() {
 }
 
 function checkingTheAnswerInOutPutTest1() {
-  let corectAnsverInOutPutTest1 = "window.alert";
+  let corectAnsverInOutPutTest1 = ".alert";
   let usersAnsverInOutPutTest1 = document.getElementById("answer").value;
   checkingTheAnswerIfThereIs1Answer(
     usersAnsverInOutPutTest1,
     corectAnsverInOutPutTest1
   );
   document.getElementById("butt").innerHTML =
-    "<button onclick = 'stylesForNewQ()' id = 'button'>Continue</button>";
+    '<button id="butt" onclick="secondQuestionInOutPut()" style = "width: 46px; height: 16px; margin: 0px; padding: 0px"> Sumbit </button>';
 }
 
 function stylesForNewQ() {
   document.getElementById("outputTests").style.backgroundColor =
     "rgb(245, 246, 250)";
-  document.getElementById("outputTests").style.paddingLeft = "5%";
-  document.getElementById("outputTests").style.paddingTop = "5%";
+  document.getElementById("outputTests").style.padding = "10px";
+  document.getElementById("outputTest").style.paddingBottom = "30px";
   document.getElementById("outputTests").style.height = "265px";
   document.getElementById("outputTests").style.margin = "10px";
   document.getElementById("outputTests").style.padding - "10px";
+  document.getElementById("outputTest").style.backgroundColor =
+    "rgb(216, 216, 216)";
+  document.getElementById("ex").innerHTML =
+    '<h2 style = "font-size: 24px; padding: 0;">Exercise:</h2>';
+  document.getElementById("task").innerHTML = "Some text";
+  document.getElementById("outputTest").innerHTML = "test";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = " ";
+}
+
+function secondQuestionInOutPut() {
+  stylesForNewQ();
+  document.getElementById("task").innerHTML = "Add the missing word";
+  document.getElementById("outputTest").innerHTML =
+    "<div 'id='outputTest'' 'class='test''>" +
+    "console" +
+    "<input style = 'margin-right:4px; height:24px; width:13%; font-size:17px; border: none;' type='text' id='answer2'/>" +
+    "('Hello World!');" +
+    "</div>";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML =
+    '<button id="butt" onclick="checkingTheAnswerInOutPutTest2()" style = "width: 46px; height: 16px; margin: 0px; padding: 0px"> Return </button>';
+}
+
+function checkingTheAnswerInOutPutTest2() {
+  let correctAnswer = ".log";
+    let userAnswer = document.getElementById("answer2").value;
+    checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer)
 }
