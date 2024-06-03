ccc15ac80b22b33bbad44e3cdf3c6698641f5a40
Liana_Kolodzheieva@example,com <liana.kolodgeeva@gmail.com>
2024-06-03 15:48:51 +0200

    added test in math

--- Js.js
+++ Js.js
@@ -6,22 +6,27 @@ function checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer) {
   }
 }
 
-function checkingTheAnswerIfThereIs2Answer(userAnswer1, userAnswer2, correctAnswer1, correctAnswer2) {
+function checkingTheAnswerIfThereIs2Answer(
+  userAnswer1,
+  userAnswer2,
+  correctAnswer1,
+  correctAnswer2
+) {
   if (userAnswer1 === correctAnswer1 && userAnswer2 === correctAnswer2) {
-    stylesIfCorrectAnswer()
+    stylesIfCorrectAnswer();
   } else {
-    stylesIfWrongAnswer()
+    stylesIfWrongAnswer();
   }
 }
 function stylesIfCorrectAnswer() {
   document.getElementById("Tests").style.backgroundColor = "#D9EEE1";
   document.getElementById("Tests").style.paddingLeft = "5%";
   document.getElementById("Tests").style.paddingTop = "5%";
-  document.getElementById("Tests").style.height = "265px";
+  document.getElementById("Tests").style.height = "325px";
   document.getElementById("Tests").style.margin = "10px";
   document.getElementById("Tests").style.padding - "10px";
   document.getElementById("ex").innerHTML =
-    '<h3 style = "color:#04AA6D"> CORRECT!</h3>'
+    '<h3 style = "color:#04AA6D"> CORRECT!</h3>';
   document.getElementById("Test").innerHTML = " ";
   document.getElementById("Test").style.backgroundColor = "#D9EEE1";
 
@@ -32,26 +37,24 @@ function stylesIfWrongAnswer() {
   document.getElementById("Tests").style.backgroundColor = "#FFC0C7";
   document.getElementById("Tests").style.paddingLeft = "5%";
   document.getElementById("Tests").style.paddingTop = "5%";
-  document.getElementById("Tests").style.height = "265px";
+  document.getElementById("Tests").style.height = "325px";
   document.getElementById("Tests").style.margin = "10px";
   document.getElementById("Tests").style.padding - "10px";
-  document.getElementById("ex").innerHTML = '<h3 style = "color:#b94a48">WRONG!</h3>'
+  document.getElementById("ex").innerHTML =
+    '<h3 style = "color:#b94a48">WRONG!</h3>';
   document.getElementById("Test").innerHTML = "";
   document.getElementById("Test").style.backgroundColor = "#FFC0C7";
   document.getElementById("Task").innerHTML = "";
-
 }
 
 function stylesForNewQ() {
-  document.getElementById("Tests").style.backgroundColor =
-    "rgb(245, 246, 250)";
+  document.getElementById("Tests").style.backgroundColor = "rgb(245, 246, 250)";
   document.getElementById("Tests").style.padding = "10px";
   document.getElementById("Test").style.paddingBottom = "30px";
-  document.getElementById("Tests").style.height = "303px";
+  document.getElementById("Tests").style.height = "325px";
   document.getElementById("Tests").style.margin = "10px";
   document.getElementById("Tests").style.padding - "10px";
-  document.getElementById("Test").style.backgroundColor =
-    "rgb(216, 216, 216)";
+  document.getElementById("Test").style.backgroundColor = "rgb(216, 216, 216)";
   document.getElementById("ex").innerHTML =
     '<h2 style = "font-size: 24px; padding: 0;">Exercise:</h2>';
   document.getElementById("Task").innerHTML = "Some text";
@@ -69,33 +72,35 @@ function checkingTheAnswerInOutPutTest1() {
     usersAnsverInOutPutTest1,
     corectAnsverInOutPutTest1
   );
-  document.getElementById("butt").innerHTML =
-    `<button id="butt" onclick="secondQuestionInOutPut()" style =     border: none;
+  document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="secondQuestionInOutPut()" style =     border: none;
   font - size: 15px;
   padding: 10px 20px;
   margin: 0;
-} "> Continue </button>`
+} "> Continue </button>`;
 }
 
 function checkingTheAnswerInOutPutTest2() {
   let correctAnswer = ".log";
   let userAnswer = document.getElementById("answer2").value;
-  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer)
-  document.getElementById("butt").innerHTML =
-    `<button id="butt" onclick="firstQuestionInOutPut()" 
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
+  document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="firstQuestionInOutPut()" 
     style = "    border: none;
     font-size: 15px;
     padding: 10px 20px;
     margin: 0;
 }"> Return </button>`;
-
 }
 
 // Output new
 
 function firstQuestionInOutPut() {
   stylesForNewQ();
-  document.getElementById("task").innerHTML = "1. Fill in the field so that the resulting text is displayed in a pop - up window";
+  document.getElementById("Task").innerHTML =
+    "1. Fill in the field so that the resulting text is displayed in a pop - up window";
   document.getElementById("Test").innerHTML =
     "<div 'id='outputTest'' 'class='test''>" +
     "window" +
@@ -104,18 +109,16 @@ function firstQuestionInOutPut() {
     "</div>";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
-  ).innerHTML =
-    `<button id="butt" onclick="checkingTheAnswerInOutPutTest1()"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInOutPutTest1()"
     style = " style = "    border: none;
     font-size: 15px;
     padding: 10px 20px;
     margin: 0;"> Sumbit </button>`;
-
 }
 
 function secondQuestionInOutPut() {
   stylesForNewQ();
-  document.getElementById("task").innerHTML = "2. Add the missing word";
+  document.getElementById("Task").innerHTML = "2. Add the missing word";
   document.getElementById("Test").innerHTML =
     "<div 'id='outputTest'' 'class='test''>" +
     "console" +
@@ -124,37 +127,79 @@ function secondQuestionInOutPut() {
     "</div>";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
-  ).innerHTML =
-    `<button id="butt" onclick="checkingTheAnswerInOutPutTest2()" style = " style = "    border: none;
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInOutPutTest2()" style = " style = "    border: none;
   font - size: 15px;
   padding: 10px 20px;
   margin: 0; "> Sumbit </button>`;
 }
 
+//operators new
+
+function firstQuestionInOperators() {
+  stylesForNewQ();
+  document.getElementById("Task").innerHTML = "1. Use the multiplying operator";
+  document.getElementById("Test").innerHTML =
+    `let x = 5;` +
+    `<br />` +
+    `let y = 2;` +
+    `<br />` +
+    `let z = x` +
+    `<input type="text" class="answer" id="answer3" />` +
+    `y; `;
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInOperatorsTest1()" style = " style = "    border: none;
+  font - size: 15px;
+  padding: 10px 20px;
+  margin: 0; "> Sumbit </button>`;
+}
+
+function secondQuestionInOperators() {
+  stylesForNewQ();
+  document.getElementById("Tests").style.height = "325px";
+  document.getElementById("Task").innerHTML = "2. Use the correct operator";
+  document.getElementById("Test").innerHTML =
+    `let user1 = "Kate";` +
+    `</br>` +
+    `let user2 = "Alina";` +
+    `</br>` +
+    `let sum = user1 <input type="text" class="answer" id="answer4" /> " " <input type="text" class="answer" id="answer5" /> user2;`;
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInOperatorsTest2()" style = " style = "    border: none;
+  font - size: 15px;
+  padding: 10px 20px;
+  margin: 0; "> Sumbit </button>`;
+}
 // //operators checking
 
 function checkingTheAnswerInOperatorsTest1() {
-  let correctAnswer = "*"
-  let userAnswer = document.getElementById("answer3")
-  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer)
+  let correctAnswer = "*";
+  let userAnswer = document.getElementById("answer3").value;
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
   document.getElementById("butt").innerHTML = document.getElementById(
-    "butt").innerHTML =
-    `<button id="butt" onclick="secondQuestionInOperators()"
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="secondQuestionInOperators()"
     style = " style = "    border: none;
     font-size: 15px;
     padding: 10px 20px;
-    margin: 0;"> Sumbit </button>`
-
+    margin: 0;"> Sumbit </button>`;
 }
 
 function checkingTheAnswerInOperatorsTest2() {
-  let userAnswer1 = document.getElementById("answer4")
-  let userAnswer2 = document.getElementById("answer5")
-  let correctAnswer1 = "+"
-  let correctAnswer2 = "+"
-  checkingTheAnswerIfThereIs2Answer(userAnswer1, userAnswer2, correctAnswer1, correctAnswer2)
-  document.getElementById("butt").innerHTML =
-    `<button id="butt" onclick="firstQuestionInOperators()"
+  let userAnswer1 = document.getElementById("answer4").value;
+  let userAnswer2 = document.getElementById("answer5").value;
+  let correctAnswer1 = "+";
+  let correctAnswer2 = "+";
+  checkingTheAnswerIfThereIs2Answer(
+    userAnswer1,
+    userAnswer2,
+    correctAnswer1,
+    correctAnswer2
+  );
+  document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="firstQuestionInOperators()"
     style = "border: none;
     font-size: 15px;
     padding: 10px 20px;
@@ -162,40 +207,56 @@ function checkingTheAnswerInOperatorsTest2() {
 }"> Return </button>`;
 }
 
-//operators new
-
-function firstQuestionInOperators() {
+//math new
+function secondQuestionInMath() {
   stylesForNewQ();
-  document.getElementById("Task").innerHTML = "1. Use the multiplying operator"
-  document.getElementById("Test").innerHTML = `let x = 5;` +
-    `<br />` +
-    `let y = 2;` +
-    `<br />` +
-    `let z = x` +
-    `<input type = "text" class="answer" id = "answer3" />` + `y; `
+  document.getElementById("Task").innerHTML =
+    "select the correct answer which will be displayed in the console:";
+  document.getElementById("exNum").innerHTML = "2. console.log(Math.ceil(6.2))";
+  document.getElementById("Test").innerHTML =
+    "Your answer:" +
+    "<br/>" +
+    "<br/>" +
+    "<input type='text' class='answer' id='answer7' />";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
-  ).innerHTML =
-    `<button id="butt" onclick="checkingTheAnswerInOperatorsTest1()       " style = " style = "    border: none;
-  font - size: 15px;
-  padding: 10px 20px;
-  margin: 0; "> Sumbit </button>`;
-
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInMathTest2()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
 }
 
-function secondQuestionInOperators() {
+function firstQuestionInMath() {
   stylesForNewQ();
-  document.getElementById("Tests").style.height = "303px"
-  document.getElementById("Task").innerHTML = "2. Use the correct operator"
-  document.getElementById("Test").innerHTML = `let user1 = "Kate";` + `</br>` +
-    `let user2 = "Alina";` + `</br>` +
-    `let sum = user1 <input type="text" class="answer" id="answer4" /> " " <input type="text" class="answer" id="answer5" /> user2;`
+}
+
+//math chrking
+
+function checkingTheAnswerInMathTest1() {
+  let userAnswer = document.getElementById("answer6").value;
+  let correctAnswer = "5";
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
+  document.getElementById("exNum").innerHTML = "";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
-  ).innerHTML =
-    `<button id="butt" onclick="checkingTheAnswerInOperatorsTest2()" style = " style = "    border: none;
-  font - size: 15px;
-  padding: 10px 20px;
-  margin: 0; "> Sumbit </button>`;
+  ).innerHTML = `<button id="butt" onclick="secondQuestionInMath()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
 }
 
+function checkingTheAnswerInMathTest2() {
+  let userAnswer = document.getElementById("answer7").value;
+  let correctAnswer = "7";
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
+  document.getElementById("exNum").innerHTML = "";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="secondQuestionInMath()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
