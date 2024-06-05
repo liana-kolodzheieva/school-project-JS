24d6bbc6d98b754ce4a529713f2c3d80547f043e
liana kolodgeeva <liana.kolodgeeva@gmail.com>
2024-06-05 22:12:44 +0200

    finish

--- Js.js
+++ Js.js
@@ -18,6 +18,23 @@ function checkingTheAnswerIfThereIs2Answer(
     stylesIfWrongAnswer();
   }
 }
+
+function checkingTheAnswerIfThereIs4Answers(
+  userAnswer1,
+  userAnswer2,
+  userAnswer3,
+  userAnswer4,
+  correctAnswer1,
+  correctAnswer2,
+  correctAnswer3,
+  correctAnswer4
+) {
+  if (userAnswer1 == correctAnswer1 && userAnswer2 == correctAnswer2 && userAnswer3 == correctAnswer3 && userAnswer4 == correctAnswer4) {
+    stylesIfCorrectAnswer();
+  } else {
+    stylesIfWrongAnswer();
+  }
+}
 function stylesIfCorrectAnswer() {
   document.getElementById("Tests").style.backgroundColor = "#D9EEE1";
   document.getElementById("Tests").style.paddingLeft = "5%";
@@ -58,7 +75,7 @@ function stylesForNewQ() {
   document.getElementById("ex").innerHTML =
     '<h2 style = "font-size: 24px; padding: 0;">Exercise:</h2>';
   document.getElementById("Task").innerHTML = "Some text";
-  document.getElementById("Test").innerHTML = "test";
+  document.getElementById("Test").innerHML = "test";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
   ).innerHTML = " ";
@@ -227,10 +244,61 @@ function secondQuestionInMath() {
     margin: 0;"> Sumbit </button>`;
 }
 
-function firstQuestionInMath() {
+function thirdQuestionInMath() {
   stylesForNewQ();
+  document.getElementById("Task").innerHTML =
+    "select the correct answer which will be displayed in the console:";
+  document.getElementById("exNum").innerHTML = "3. console.log(Math.floor(2.7))";
+  document.getElementById("Test").innerHTML =
+    "Your answer:" +
+    "<br/>" +
+    "<br/>" +
+    "<input type='text' class='answer' id='answer8' />";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInMathTest3()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
 }
 
+function firstQuestionInMath() {
+  stylesForNewQ();
+  document.getElementById("Task").innerHTML =
+    "select the correct answer which will be displayed in the console:";
+  document.getElementById("exNum").innerHTML = "1. console.log(Math.round(4.6))";
+  document.getElementById("Test").innerHTML =
+    "Your answer:" +
+    "<br/>" +
+    "<br/>" +
+    "<input type='text' class='answer' id='answer7' />";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInMathTest1()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
+function fourthQuestionInMath() {
+  stylesForNewQ();
+  document.getElementById("Task").innerHTML =
+    "select the correct answer which will be displayed in the console:";
+  document.getElementById("exNum").innerHTML = "1. console.log(Math.trunc(2.1))";
+  document.getElementById("Test").innerHTML =
+    "Your answer:" +
+    "<br/>" +
+    "<br/>" +
+    "<input type='text' class='answer' id='answer9' />";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInMathTest4()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
 //math chrking
 
 function checkingTheAnswerInMathTest1() {
@@ -254,7 +322,109 @@ function checkingTheAnswerInMathTest2() {
   document.getElementById("exNum").innerHTML = "";
   document.getElementById("butt").innerHTML = document.getElementById(
     "butt"
-  ).innerHTML = `<button id="butt" onclick="secondQuestionInMath()"
+  ).innerHTML = `<button id="butt" onclick="thirdQuestionInMath()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
+
+function checkingTheAnswerInMathTest3() {
+  let userAnswer = document.getElementById("answer8").value;
+  let correctAnswer = "2";
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
+  document.getElementById("exNum").innerHTML = "";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="fourthQuestionInMath()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
+
+function checkingTheAnswerInMathTest4() {
+  let userAnswer = document.getElementById("answer9").value;
+  let correctAnswer = "2";
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer);
+  document.getElementById("exNum").innerHTML = "";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="firstQuestionInMath()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Return </button>`;
+}
+
+//function checking
+function checkingTheAnswerInFunctionTest1() {
+  let userAnswer = document.getElementById("answer10").value
+  let correctAnswer = "myFunction()"
+  checkingTheAnswerIfThereIs1Answer(userAnswer, correctAnswer)
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="secondQuestionInFunction()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
+function checkingTheAnswerInFunctionTest2() {
+  let userAnswer = document.getElementById("answer11").value
+  let userAnswer2 = document.getElementById("answer12").value
+  let userAnswer3 = document.getElementById("answer13").value
+  let userAnswer4 = document.getElementById("answer14").value
+  let correctAnswer = "function"
+  let correctAnswer2 = "myFunction()"
+  let correctAnswer3 = "{"
+  let correctAnswer4 = "}"
+  checkingTheAnswerIfThereIs4Answers(userAnswer, userAnswer2, userAnswer3, userAnswer4, correctAnswer, correctAnswer2, correctAnswer3, correctAnswer4)
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="firstQuestionInFunction()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Return </button>`;
+}
+
+
+//function new
+
+function secondQuestionInFunction() {
+  stylesForNewQ();
+  document.getElementById("Task").innerHTML =
+    "2. Execute the function named myFunction";
+  document.getElementById("Test").innerHTML =
+    "<input type='text' class='answer' id='answer11' />" +
+    "<input type='text' class='answer' id='answer12' />" +
+    "<input type='text' class='answer' id='answer13' />" +
+    '<p>alert("Hello World!");</p>' +
+    "<input type='text' class='answer' id='answer14' />"
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInFunctionTest2()"
+    style = " style = "    border: none;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin: 0;"> Sumbit </button>`;
+}
+
+
+
+function firstQuestionInFunction() {
+  stylesForNewQ();
+  document.getElementById("Task").innerHTML =
+    "1. Execute the function";
+  document.getElementById("Test").innerHTML =
+    "<p>function myFunction() {<p/>" +
+    "<p>alert('Hello World!');<p/>" +
+    "<p>}<p/>" +
+    "<input type='text' class='answer' id='answer10' />";
+  document.getElementById("butt").innerHTML = document.getElementById(
+    "butt"
+  ).innerHTML = `<button id="butt" onclick="checkingTheAnswerInFunctionTest1()"
     style = " style = "    border: none;
     font-size: 15px;
     padding: 10px 20px;
